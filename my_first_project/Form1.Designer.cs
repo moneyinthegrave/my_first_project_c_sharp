49c7e80f6f0a7e71c50c428b29e8b0af00787631
self care <49820562+ICANSTILLHEARYOU@users.noreply.github.com>
2022-01-22 15:34:43 +0300

    Initial commit

---
+++ my_first_project/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace my_first_project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(283, 341);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 32);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(258, 136);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(139, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(207, 278);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(139, 14);
+            this.label1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(258, 205);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(139, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(124, 211);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 28);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Конец интервала";
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(124, 139);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 30);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Начало интервала";
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(50, 278);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(162, 26);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Сумма числе в интервале:\r\n";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.Label label4;
+
+        private System.Windows.Forms.Label label3;
+
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+
+        #endregion
+    }
+}
---
+++ my_first_project/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace my_first_project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // получаем две строки из текстбокса
+            string frist_num = textBox1.Text;
+            string second_num = textBox2.Text;
+            
+            // создаем переменные для заполнения и создания массива
+            int Start_of_array = Convert.ToInt32(frist_num);
+            int end_of_array = Convert.ToInt32(second_num);
+            
+            // interval_length размерность массива
+            int interval_length = (end_of_array - Start_of_array) + 1;
+            
+            int sum = 0;
+            
+            //создаем динаимческий массив
+            int [] Array = new int [interval_length];
+            
+            // заполняем массив
+            for (int i = 0; i < interval_length  ; i++)
+            {
+                Array[i] = Start_of_array;
+                Start_of_array += 1;
+            }
+            // складываем элементы массива в переменную sum
+            for (int i = 0; i < interval_length ; i++)
+            {
+                sum += Array[i];
+            }
+            // вывод в лейбл
+            label1.Text = Convert.ToString(sum);
+        }
+    }
+}
